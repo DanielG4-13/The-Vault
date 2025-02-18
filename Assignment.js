b2e08c6008b0651543d7e2fca76c0e0433c0c063
Daniel Garibay <danielgaribay366@gmail.com>
2025-02-18 12:27:43 -0600

    Commit

---
+++ Assignment.js
@@ -0,0 +1,11 @@
+// I assigned '(19-12)+3' to the variable 'num1'
+let num1 = (19-12)+3(num1+'-'+num2+'-'+num3)
+// I assigned 2 times 20 to the variable 'num2'
+let num2 = 2*20
+// I assigned 36+3 to the variable 'num3'
+let num3 = 36+3
+// I concatenated num1, num2, num2, and '-' to the variable answer
+let answer= (num1+'-'+num2+'-'+num3)
+// I concatenated a string to the variable answer
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination: "+ answer);
+
